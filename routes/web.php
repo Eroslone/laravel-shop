1794b4d319ed1d90e2550d192a6f3e41b21459ee
Eroslone <3079758191@qq.com>
2022-09-06 03:27:45 +0000

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 // 在之前的路由后面配上中间件
 //Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
-Route::redirect('/', '/products')->name('root');
-Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 
 // 在之前的路由里加上一个 verify 参数
 Auth::routes(['verify' => true]);
@@ -14,6 +12,7 @@ Auth::routes(['verify' => true]);
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
     Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
     Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
@@ -22,11 +21,18 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+
+
+
     Route::post('cart', 'CartController@add')->name('cart.add');
     Route::get('cart', 'CartController@index')->name('cart.index');
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
     Route::post('orders', 'OrdersController@store')->name('orders.store');
     Route::get('orders', 'OrdersController@index')->name('orders.index');
+    Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
 
 
 });
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
