9a6ce6e8223f27ff505569f861d7c0661783e110
Eroslone <3079758191@qq.com>
2022-09-07 07:44:37 +0000

    评价商品

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -12,6 +12,8 @@ use App\Jobs\CloseOrder;
 use Illuminate\Http\Request;
 use App\Services\CartService;
 use App\Services\OrderService;
+use App\Http\Requests\SendReviewRequest;
+use App\Events\OrderReviewed;
 
 class OrdersController extends Controller
 {
@@ -31,61 +33,7 @@ class OrdersController extends Controller
         $this->authorize('own', $order);
         return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
     }
-    /*public function store(OrderRequest $request, CartService $cartService, OrderService $orderService)
-    {
-        $user  = $request->user();
-        // 开启一个数据库事务
-        $order = \DB::transaction(function () use ($user, $request, $cartService) {
-            $address = UserAddress::find($request->input('address_id'));
-            // 更新此地址的最后使用时间
-            $address->update(['last_used_at' => Carbon::now()]);
-            // 创建一个订单
-            $order = new Order([
-                'address' => [ // 将地址信息放入订单中
-                    'address' => $address->full_address,
-                    'zip' => $address->zip,
-                    'contact_name' => $address->contact_name,
-                    'contact_phone' => $address->contact_phone,
-                ],
-                'remark' => $request->input('remark'),
-                'total_amount' => 0,
-            ]);
-            // 订单关联到当前用户
-            $order->user()->associate($user);
-            // 写入数据库
-            $order->save();
 
-            $totalAmount = 0;
-            $items = $request->input('items');
-            // 遍历用户提交的 SKU
-            foreach ($items as $data) {
-                $sku  = ProductSku::find($data['sku_id']);
-                // 创建一个 OrderItem 并直接与当前订单关联
-                $item = $order->items()->make([
-                    'amount' => $data['amount'],
-                    'price' => $sku->price,
-                ]);
-                $item->product()->associate($sku->product_id);
-                $item->productSku()->associate($sku);
-                $item->save();
-                $totalAmount += $sku->price * $data['amount'];
-                if ($sku->decreaseStock($data['amount']) <= 0) {
-                    throw new InvalidRequestException('该商品库存不足');
-                }
-            }
-
-            // 更新订单总金额
-            $order->update(['total_amount' => $totalAmount]);
-
-            // 将下单的商品从购物车中移除
-            $skuIds = collect($request->input('items'))->pluck('sku_id')->all();
-            $cartService->remove($skuIds);
-
-            return $order;
-        });
-        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
-        return $order;
-    }*/
     public function store(OrderRequest $request, OrderService $orderService)
     {
         $user    = $request->user();
@@ -108,5 +56,47 @@ class OrdersController extends Controller
 
         // 返回订单信息
         return $order;
+    }
+    public function review(Order $order)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+        // 判断是否已经支付
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
+        }
+        // 使用 load 方法加载关联数据，避免 N + 1 性能问题
+        return view('orders.review', ['order' => $order->load(['items.productSku', 'items.product'])]);
+    }
+
+    public function sendReview(Order $order, SendReviewRequest $request)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
+        }
+        // 判断是否已经评价
+        if ($order->reviewed) {
+            throw new InvalidRequestException('该订单已评价，不可重复提交');
+        }
+        $reviews = $request->input('reviews');
+        // 开启事务
+        \DB::transaction(function () use ($reviews, $order) {
+            // 遍历用户提交的数据
+            foreach ($reviews as $review) {
+                $orderItem = $order->items()->find($review['id']);
+                // 保存评分和评价
+                $orderItem->update([
+                    'rating'      => $review['rating'],
+                    'review'      => $review['review'],
+                    'reviewed_at' => Carbon::now(),
+                ]);
+            }
+            // 将订单标记为已评价
+            $order->update(['reviewed' => true]);
+        });
+        event(new OrderReviewed($order));
+        return redirect()->back();
     }
 }
--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Exceptions\InvalidRequestException;
+use App\Models\OrderItem;
 
 class ProductsController extends Controller
 {
@@ -15,7 +16,7 @@ class ProductsController extends Controller
         // 判断是否有提交 search 参数，如果有就赋值给 $search 变量
         // search 参数用来模糊搜索商品
         if ($search = $request->input('search', '')) {
-            $like = '%' . $search . '%';
+            $like = '%'.$search.'%';
             // 模糊搜索商品标题、商品详情、SKU 标题、SKU描述
             $builder->where(function ($query) use ($like) {
                 $query->where('title', 'like', $like)
@@ -50,6 +51,7 @@ class ProductsController extends Controller
             ],
         ]);
     }
+
     public function show(Product $product, Request $request)
     {
         if (!$product->on_sale) {
@@ -64,8 +66,22 @@ class ProductsController extends Controller
             $favored = boolval($user->favoriteProducts()->find($product->id));
         }
 
-        return view('products.show', ['product' => $product, 'favored' => $favored]);
+        $reviews = OrderItem::query()
+            ->with(['order.user', 'productSku']) // 预先加载关联关系
+            ->where('product_id', $product->id)
+            ->whereNotNull('reviewed_at') // 筛选出已评价的
+            ->orderBy('reviewed_at', 'desc') // 按评价时间倒序
+            ->limit(10) // 取出 10 条
+            ->get();
+
+        // 最后别忘了注入到模板中
+        return view('products.show', [
+            'product' => $product,
+            'favored' => $favored,
+            'reviews' => $reviews
+        ]);
     }
+
     public function favor(Product $product, Request $request)
     {
         $user = $request->user();
@@ -77,6 +93,7 @@ class ProductsController extends Controller
 
         return [];
     }
+
     public function disfavor(Product $product, Request $request)
     {
         $user = $request->user();
@@ -84,6 +101,7 @@ class ProductsController extends Controller
 
         return [];
     }
+
     public function favorites(Request $request)
     {
         $products = $request->user()->favoriteProducts()->paginate(16);
