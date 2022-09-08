faff488a57a62d0f10988500db45567b15c8a4e7
Eroslone <3079758191@qq.com>
2022-09-08 08:03:58 +0000

    初始化众筹模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,9 +9,15 @@ use Illuminate\Support\Str;
 class Product extends Model
 {
     use HasFactory;
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+    public static $typeMap = [
+        self::TYPE_NORMAL  => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
-        'rating', 'sold_count', 'review_count', 'price','category_id'
+        'rating', 'sold_count', 'review_count', 'price','category_id','type',
     ];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
@@ -32,5 +38,9 @@ class Product extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+    public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
     }
 }
