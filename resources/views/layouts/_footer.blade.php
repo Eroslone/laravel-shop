ce3258cc7b082b198787d24548c2167d6e2a41e2
Eroslone <3079758191@qq.com>
2022-09-29 13:56:56 +0800

    添加备案号

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,7 +3,9 @@
     <p class="float-start">
       由 <a href="https://leo108.com" target="_blank">Leo</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
-
+        <p class="float-start">
+      备案号:<a href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备2022004954号-1</a>
+    </p>
     <p class="float-end"><a href="mailto:name@email.com">联系我们</a></p>
   </div>
 </footer>
